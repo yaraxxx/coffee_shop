be35749be838537cdb682519d5da1ebb4e52f3bf
Yara <42673315+yarasaleh@users.noreply.github.com>
2020-08-22 14:44:43 +0300

    Fixed : auth0 url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'udacity-full-stack.us.auth0.com', // the auth0 domain prefix
+    url: 'udacity-full-stack.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'D5gAtIeiy7FnW6ynuhVW1SABBqJNu005', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100/', // the base url of the running ionic application.
